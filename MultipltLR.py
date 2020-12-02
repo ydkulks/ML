e9b8fef34a1c93b7eae1a48e3360f3888b9e9014
ydkulks <ydkulks2@gmail.com>
2020-12-02 21:49:22 +0530

    Added MLR

---
+++ MultipltLR.py
@@ -0,0 +1,12 @@
+#Multiple Linear Regression
+import matplotlib as mp
+mp.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
